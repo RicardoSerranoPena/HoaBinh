53aaf43c62e2d199adbb45171fd16e55d21198f4
Ricardo A. Serrano Pena <ricardo30499@gmail.com>
2024-07-10 09:16:12 +0700

    added recent blogs html + css to home page

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,65 @@ const appConfig = useAppConfig()
         văn hóa đạo đức
       </h1>
     </div>
+
+    <section class="recent-blogs">
+      <h2>Recent Blogs</h2>
+      <div class="cards">
+        <div class="card">
+          <div class="card-img-wrapper">
+            <img class="card-img" src="~/assets/images/hb-medium.webp" />
+          </div>
+          <p class="date">Sunday 1 Jan 2023</p>
+          <h3 class="card-title">Giáo dục từ trái tim</h3>
+          <p class="card-body">
+            Nhà trường luôn chú trọng đến đời sống của các cô, để cùng sống với
+            đạo đức thương mình, thương người, một tình thương rộng lớn phủ trùm
+            vũ trụ, vạn vật để nuôi dưỡng các bé cũng có tình yêu thương như
+            vậy.
+          </p>
+          <div class="labels">
+            <span class="label">Design</span>
+            <span class="label">Research</span>
+            <span class="label">Resource</span>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-img-wrapper">
+            <img class="card-img" src="~/assets/images/hb-medium.webp" />
+          </div>
+          <p class="date">Sunday 1 Jan 2023</p>
+          <h3 class="card-title">Vật liệu tự nhiên</h3>
+          <p class="card-body">
+            Đồ chơi tại trường hoàn toàn làm bằn`g đôi bàn tay mẹ, của cha và
+            của cô giáo để khơi gợi lòng biết ơn của trẻ. Vật liệu được nhập
+            khẩu từ nước ngoài, 100% làm từ nguyên liệu thân thiện với môi
+            trường.
+          </p>
+          <div class="labels">
+            <span class="label">Design</span>
+            <span class="label">Research</span>
+            <span class="label">Resource</span>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-img-wrapper">
+            <img class="card-img" src="~/assets/images/hb-medium.webp" />
+          </div>
+          <p class="date">Sunday 1 Jan 2023</p>
+          <h3 class="card-title">Ngôi trường hạnh phúc</h3>
+          <p class="card-body">
+            Chúng tôi mong muốn xây dựng một ngôi trường hạnh phúc, để trẻ được
+            an toàn, tự do sáng tạo, gần gũi với thiên nhiên, tập yêu thương con
+            người, động vật và cả cây cỏ hoa lá.
+          </p>
+          <div class="labels">
+            <span class="label">Design</span>
+            <span class="label">Research</span>
+            <span class="label">Resource</span>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
